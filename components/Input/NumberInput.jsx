c785c52e6dec92b466041d489a8c50a54dfffefc
laoTing <1375500429@qq.com>
2022-03-25 19:25:47 +0800

    1. 货币转换器 计算器初步完成

--- components/Input/NumberInput.jsx
+++ components/Input/NumberInput.jsx
@@ -1,67 +1,142 @@
-import React, {useState} from 'react'
-import { Input } from 'antd';
+import React, { createRef, useEffect, useState } from 'react'
 
-export default function NumberInput() {
-  const [inputVal, setInputVal1] = useState('')
-  const [isComposition, setIsComposition] = useState(false)
+export default function NumberInput(props) {
+  const inputRef = createRef()
+  const { value, onChangeInput, currencytype} = props
+  var iValue = currencytype.value
+  let isComposition = false
 
   // 更改value 值
   const changeInputValue = (e) => {
     if (!isComposition) {
       let { value } = e.target
-      var len = value.split(".").length-1
+      var len = value.split(".").length - 1
       if (len > 1) {
+        inputRef.current.value = iValue
         return
-      } else if (value === '.') {
+      } else if (value === '.' ) {
         value = '0.'
       }
       value = value.replace(/[^\d{1,}\.\d{1,}|\d{1,}]/g, '')
-      if (value !== inputVal && value.indexOf()) {
-        setInputVal1(value)
-      }
+      iValue = value
+      e.target.value = iValue
+      iValue !== '' && onChangeInput({
+        currencytype,
+        value: parseInt(iValue)
+      })
     }
   }
+  // 初始化input的值
+  const setInput = () => {
+    inputRef.current.value = iValue
+  }
+  useEffect(() => {
+    console.log('更新')
+    setInput()
+  })
   // 判断是否输入法输入
   const handleComposition = (ev) => {
+    console.log(ev.type)
     if (ev.type === 'compositionend') {
-      setIsComposition(false)
+      isComposition = false
       let isChrome = window.navigator.userAgent.indexOf('Chrome') > -1
       if (!isComposition && isChrome) {
         changeInputValue(ev)
       }
     } else {
-      console.log(ev.type)
-      setIsComposition(true)
+      isComposition = true
     }
   }
    // 千分位转换
-  const currencyPrices = (str) => {
-    str = typeof str == 'number' ? String(str) : str
-    str.length > 15 ? str.substr(0, 15) : str
-    if (!str) return
-    // 判断是否有小数掉
-    if (str.indexOf('.') !== -1) {
-      // 小数点出现的位置
-      var strInterception = str.indexOf(".")
-      // 截取小数点前部分数据
-      var start = str.substr(0, strInterception)
-      // 截取小数点后部分数据
-      var end = str.substr(strInterception, str.length)
-      return parseFloat(start).toLocaleString("en-US") + end
-    } else {
-      return parseFloat(str).toLocaleString("en-US")
-    }
-  }
+  // const currencyPrices = (str) => {
+  //   str = typeof str == 'number' ? String(str) : str
+  //   str.length > 15 ? str.substr(0, 15) : str
+  //   if (!str) return
+  //   // 判断是否有小数掉
+  //   if (str.indexOf('.') !== -1) {
+  //     // 小数点出现的位置
+  //     var strInterception = str.indexOf(".")
+  //     // 截取小数点前部分数据
+  //     var start = str.substr(0, strInterception)
+  //     // 截取小数点后部分数据
+  //     var end = str.substr(strInterception, str.length)
+  //     return parseFloat(start).toLocaleString("en-US") + end
+  //   } else {
+  //     return parseFloat(str).toLocaleString("en-US")
+  //   }
+  // }
 
   return (
     <>
-      <Input
+      <input
+        className='ant-input'
         type='text'
+        ref={ inputRef }
         onChange={changeInputValue}
-        onCompositionStart={handleComposition}
-        onCompositionEnd={handleComposition}
-        value={inputVal}
-      ></Input>
+        // onCompositionStart={handleComposition}
+        // onCompositionEnd={handleComposition}
+      ></input>
     </>
   )
 }
+
+
+// class NumberInput extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       value1: ''
+//     }
+//     this.handleChange1 = this.handleChange1.bind(this)
+//     this.handleChange2 = this.handleChange2.bind(this)
+//     this.handleComposition = this.handleComposition.bind(this)
+//   }
+//   static isComposition = false
+
+//   handleChange1 = ev => {
+//     this.setState({
+//       value1: ev.target.value,
+//     })
+//   }
+
+//   handleChange2 = ev => {
+//     // 未使用输入法或使用输入法完毕才能触发
+//     console.log(this.isComposition)
+//     if (!this.isComposition) {
+//       this.setState({
+//         value2: ev.target.value,
+//       })
+//     }
+//   }
+
+//   handleComposition(ev) {
+//     if (ev.type === 'compositionend') {
+//       const isChrome = navigator.userAgent.indexOf('Chrome') > -1
+//       this.setState({
+//         isComposition: false
+//       })
+//       isComposition= false
+//       if (!isComposition && isChrome) {
+//         this.handleChange2(ev)
+//       }
+//     } else {
+//       isComposition = true
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Input
+//           type='text'
+//           onChange={this.handleChange2}
+//           onCompositionStart={this.handleComposition}
+//           onCompositionEnd={this.handleComposition}
+//           placeholder='使用了composition的input框'
+//         />
+//       </div>
+//     )
+//   }
+// }
+
+// export default NumberInput
--- pages/input/index.jsx
+++ pages/input/index.jsx
@@ -1,63 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react';
+import PropTypes from 'prop-types';
 
-let isComposition = false
+export default class InputDemo extends Component {
 
-class TestComposition extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value1: '',
-      value2: '',
+    inputRef = createRef();
+
+    isOnComposition = false;
+
+    componentDidMount() {
+        this.setInputValue();
     }
-    this.handleChange1 = this.handleChange1.bind(this)
-    this.handleChange2 = this.handleChange2.bind(this)
-    this.handleComposition = this.handleComposition.bind(this)
-  }
 
-  handleChange1 = ev => {
-    this.setState({
-      value1: ev.target.value,
-    })
-  }
-
-  handleChange2 = ev => {
-    // 未使用输入法或使用输入法完毕才能触发
-    console.log(isComposition)
-    if (!isComposition) {
-      this.setState({
-        value2: ev.target.value,
-      })
+    componentDidUpdate() {
+        this.setInputValue();
     }
-  }
 
-  handleComposition(ev) {
-    if (ev.type === 'compositionend') {
-      const isChrome = navigator.userAgent.indexOf('Chrome') > -1
-      isComposition = false
-      if (!isComposition && isChrome) {
-        this.handleChange2(ev)
-      }
-    } else {
-      isComposition = true
-    }
-  }
+    setInputValue = () => {
+        this.inputRef.current.value = this.props.value || ''
+    };
 
-  render() {
-    return (
-      <div>
-        <input type='text' onChange={this.handleChange1} />
-        <span>{this.state.value1}</span>
-        <input
-          type='text'
-          onChange={this.handleChange2}
-          onCompositionStart={this.handleComposition}
-          onCompositionEnd={this.handleComposition}
-          placeholder='使用了composition的input框'
+    handleComposition = evt => {
+        if (evt.type === 'compositionend') {
+            this.isOnComposition = false;
+            return;
+        }
+
+        this.isOnComposition = true;
+    };
+
+    onChange = evt => {
+        if (!this.isOnComposition) {
+            this.props.onChange(evt);
+        }
+    };
+
+    render() {
+        const commonProps = {
+            onChange: this.onChange,
+            onCompositionStart: this.handleComposition,
+            onCompositionUpdate: this.handleComposition,
+            onCompositionEnd: this.handleComposition,
+        };
+        return <input
+            ref={this.inputRef}
+            type="text"
+            {...commonProps}
         />
-        <span>{this.state.value2}</span>
-      </div>
-    )
-  }
+    }
 }
-
-export default TestComposition
